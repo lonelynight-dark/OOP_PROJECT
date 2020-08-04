db3297199d69bf9319aa352fed598d317059d305
minhthanh206lhp <63918957+minhthanh206lhp@users.noreply.github.com>
2020-08-04 20:24:34 +0700

    Small Manager functions added
    addStaff, Manager input/output, viewProduct, viewEmployee

--- OOP_PROJECT/OOP_PROJECT/Manager.cpp
+++ OOP_PROJECT/OOP_PROJECT/Manager.cpp
@@ -1,1 +1,28 @@
 #include "Manager.h"
+
+void Manager::addStaff()
+{
+	Employee* newEmployee = new Employee;
+	newEmployee->input();
+	List.push_back(newEmployee);
+}
+
+void Manager::input()
+{
+	Staff::input();
+}
+
+void Manager::output()
+{
+	Staff::output();
+}
+
+void Manager::viewProd()
+{
+	prodList.outputList();
+}
+
+void Manager::viewEmp()
+{
+	for (int i = 0; i < nEmp; i++) List[i]->output();
+}
--- OOP_PROJECT/OOP_PROJECT/Manager.h
+++ OOP_PROJECT/OOP_PROJECT/Manager.h
@@ -1,10 +1,24 @@
 #ifndef _MANAGER_H_
 #define _MANGAGER_H_
 
+#include "Employee.h"
 #include "Staff.h"
-class Manager:
-	public Staff
+#include <vector>
+
+class Manager: public Staff
 {
+public:
+	Manager() :Staff() { nEmp = 0; }
+	~Manager() { for (int i = 0; i < nEmp; i++) delete List[i]; }
+	void addStaff();
+	void input();
+	void output();
+	void viewProd();
+	void viewEmp();
+private:
+	std::vector<Employee*> List;
+	ListProduct prodList;
+	int nEmp;
 };
 
 #endif // !_MANAGER_H_
